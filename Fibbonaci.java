7eb30afa595f2580d0947ab16c6a43897e1a6e80
namita1699 <50766458+namita1699@users.noreply.github.com>
2019-05-21 08:53:20 -0700

    Add files via upload

---
+++ Fibbonaci.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Fibbonaci
+{
+	public static void main(String args[])
+	{
+		int n1=0,n2=1,n3,n;
+		System.out.println("Enter number");
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextInt();
+		for(int i=2;i<=n;i++)
+		{
+			n3=n1+n2;
+			System.out.println(n1+"\n");
+			n1=n2;
+			n2=n3;
+			
+		}
+		
+		
+	}
+}
